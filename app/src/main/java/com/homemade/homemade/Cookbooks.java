ac4118bb6cb07fe256d77f8ab4ccbc38af9ca51f
brianhicks <b.hicks1996@gmail.com>
2018-02-06 17:02:17 -0800

    Added empty activities for each activity

---
+++ app/src/main/java/com/homemade/homemade/Cookbooks.java
@@ -0,0 +1,13 @@
+package com.homemade.homemade;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cookbooks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cookbooks);
+    }
+}
