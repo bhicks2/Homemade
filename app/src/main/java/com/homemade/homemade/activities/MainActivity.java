4796587977f52ba7e03f55061a9beac5be5ab64d
brianhicks <b.hicks1996@gmail.com>
2018-02-11 16:51:10 -0800

    Basic setup implemented

--- app/src/main/java/com/homemade/homemade/activities/MainActivity.java
+++ app/src/main/java/com/homemade/homemade/activities/MainActivity.java
@@ -1,9 +1,15 @@
 package com.homemade.homemade.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.homemade.homemade.R;
 
@@ -18,12 +24,31 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<String> recipesList = new ArrayList<>();
         recipesList.add("Potato");
-        recipesList.add("Water");
-        recipesList.add("Syracuse");
+        recipesList.add("Watermelon");
+        recipesList.add("Qumquat");
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_main, recipesList);
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, recipesList);
 
-        ListView listView = (ListView) findViewById(R.id.recipe_list);
-        listView.setAdapter(adapter);
+        ListView recipes = (ListView) findViewById(R.id.recipe_list);
+        recipes.setAdapter(adapter);
+
+        recipes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView recipes = (TextView) view;
+                String recipeName = (String) recipes.getText();
+
+                Toast.makeText(getApplicationContext(), recipeName + " selected", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), SingleRecipe.class);
+                intent.putExtra("RECIPE_NAME", recipeName);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void goToRecipe(View view) {
+
+        Intent intent = new Intent(this, SingleRecipe.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/homemade/homemade/activities/SingleRecipe.java
+++ app/src/main/java/com/homemade/homemade/activities/SingleRecipe.java
@@ -1,7 +1,10 @@
 package com.homemade.homemade.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.homemade.homemade.R;
 
@@ -11,5 +14,14 @@ public class SingleRecipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_recipe);
+
+        Intent intent = getIntent();
+
+        String recipeName = intent.getStringExtra("RECIPE_NAME");
+
+        ImageView imgView = (ImageView)findViewById(R.id.recipe_image);
+        imgView.setImageResource(R.drawable.potato);
+        TextView title = (TextView)findViewById(R.id.recipe_name);
+        title.setText(recipeName);
     }
 }
