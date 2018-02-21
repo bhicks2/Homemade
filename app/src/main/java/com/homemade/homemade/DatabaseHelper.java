8bc4868d3940299caf4abc92b7f6792a0dd11548
Stan Chernavsky <stanmc@stanford.edu>
2018-02-20 17:53:42 -0800

    Created DatabaseHelper class

---
+++ app/src/main/java/com/homemade/homemade/DatabaseHelper.java
@@ -0,0 +1,20 @@
+package com.homemade.homemade;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Stan on 2/20/18.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
